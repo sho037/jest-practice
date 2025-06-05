6257b885409e16a7c354d13be33a28aa37bdc875
sho037 <103867061+sho037@users.noreply.github.com>
2025-06-05 13:57:57 +0900

    dev: linterとformatterを導入

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import { FlatCompat } from '@eslint/eslintrc'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  tseslint.configs.recommended,
+
+  ...compat.extends('prettier'),
+])
